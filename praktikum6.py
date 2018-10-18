981130fbe75fa4ad084eea2c61dcc829292dbd12
L200180153 <44190314+L200180153@users.noreply.github.com>
2018-10-18 05:23:51 -0700

    Add files via upload

---
+++ praktikum6.py
@@ -0,0 +1,25 @@
+## Kegiatan 1
+## program Identitas diri. dibuat oleh Anita L200180153
+Nama_panjang = 'Anita Lusi Romadhon'
+Nama_singkat = 'Anita'
+NIM = 'L200180153'
+Alamat = 'Sragen'
+Tempat_tinggal = 'Nila Sari'
+Fakultas = 'FKI'
+Prodi = 'Informatika'
+Kelas = 'D'
+TTl = 'Sragen,02Januari2000'
+Hobi = 'membaca'
+
+## Kegiatan 2
+## Program Akun
+## Dibuat oleh Anita L200180153
+Nama = 'Anita Lusi Romadhon'
+Tanggal_lahir = '02 Januari 2000'
+Nama_singkat = Nama [0:1] + "." + Nama [6:7] + "." + Nama [11:]
+Username = Nama [0:1] + Tanggal_lahir [0:2] + Tanggal_lahir [11:]
+Password = Nama [0:3] + "041"
+
+
+
+    
